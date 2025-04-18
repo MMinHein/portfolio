60baa2a6dee2f6fc71eb39874ecfec76ae05f45c
mmh <minheinmyat007@gmail.com>
2025-04-18 21:15:00 +0630

    created new file named Mobilemenu.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { LoadingScreen } from "./components/LoadingScreen";
 import { Navbar } from "./components/Navbar";
+import { MobileMenu } from "./components/MobileMenu";
 function App() {
   const [loaded, setLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <>
       {!loaded && <LoadingScreen onComplete={() => setLoaded(true)} />}
@@ -11,7 +13,8 @@ function App() {
           loaded ? "opacity-100" : "opacity-0"
         } bg-black text-gray-100`}
       >
-        <Navbar />
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       </div>
     </>
   );
